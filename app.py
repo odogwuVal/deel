8049c6b541e2ee3c1ac622e4a8516010d860d052
valentine <vmadu@gammax.exchange>
2023-09-13 09:17:06 +0100

    wip

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 from sqlalchemy.exc import SQLAlchemyError
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
@@ -8,6 +9,7 @@ from models import ReverseIP
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///data.db")
 db.init_app(app)
@@ -18,14 +20,17 @@ with app.app_context():
 
 @app.route('/')
 def get_reverse_ip():
-    client_ip = request.remote_addr
-    reverse_ip = '.'.join(reversed(client_ip.split('.')))
-    reversed_ip_obj = ReverseIP(original_ip=client_ip, reversed_ip=reverse_ip)
+    client_ip = request.headers.get('X-Forwarded-For')
+    if client_ip:
+        reverse_ip = '.'.join(reversed(client_ip.split('.')))
+        reversed_ip_obj = ReverseIP(original_ip=client_ip, reversed_ip=reverse_ip)
 
-    try:
-        db.session.add(reversed_ip_obj)
-        db.session.commit()
-        return f"Reversed IP: {reverse_ip} is Stored"
-    except Exception as e:
-        db.session.rollback()
-        return f"Error: {str(e)}"
+        try:
+            db.session.add(reversed_ip_obj)
+            db.session.commit()
+            return f"Reversed IP: {reverse_ip} is Stored"
+        except Exception as e:
+            db.session.rollback()
+            return f"Error: {str(e)}"
+    else:
+        return "X-Forwarded-For header not found in the request."
